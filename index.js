a6be37651456927edb7b53293754aa1079465042
AyanAB <ayantorres@gmail.com>
2018-04-05 19:20:35 +0530

    Testing EJS

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.get('/', function(req,res){
-    res.sendFile(__dirname + "/page-login.html");
+    res.render("indextest");
 });
 
 app.listen(PORT,
