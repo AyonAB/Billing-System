9b288c1a5ffa9ad7e14894ff748777b08ceece75
AyanAB <ayantorres@gmail.com>
2018-04-05 19:53:05 +0530

    Testing to get css files in heroku

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req,res){
     res.render("login");
 });
