5fa87fa81a172ff42ce7f600a19acef3fbc177fb
AyanAB <ayantorres@gmail.com>
2018-04-06 01:19:16 +0530

    Minor Change

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+// Imports
 const express = require('express');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
+// Setting up express
 var app = express();
 
+// Template Engine
 app.set('view engine', 'ejs');
+
+// Middlewares for Static files
 app.use('/assets', express.static('assets'));
 app.use('/images', express.static('images'));
+
+// Routes
 app.get('/', function(req,res){
+    res.render("page-login");
+});
+
+app.get('/dashboard', function(req,res){
     res.render("index");
 });
 
+// Port deploy & debug
 app.listen(PORT,
   function(){
     console.log("Server listening on port " + PORT);
