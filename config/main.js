33ca8d1f5b6c8a87490a4fb691f6945c3719e8fd
ayantorres <ayantorres@gmail.com>
2018-04-13 16:05:10 -0400

    Updated for testing of routes

--- config/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {  
-  // Secret key for JWT signing and encryption
-  'secret': 'super secret passphrase',
-  // Database connection information
-  'database': 'mongodb://AyonAB:Ayan1996@ds241489.mlab.com:41489/billing-system',
-  // Setting port for server
-  'port': process.env.PORT || 5000
-}
