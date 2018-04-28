9a5d8e8e772964de3dabd45e769da0b36d551834
susanta96 <susant.vanu7278@gmail.com>
2018-04-28 19:19:01 +0530

    don't know

--- model/bill.js
+++ model/bill.js
@@ -4,18 +4,18 @@ var Schema = mongoose.Schema;
 //create schema for product
 var bill = new Schema({
     name:{type: String, required: [true, '{PATH} is required']},
-    cname: {type: String, required: [true, '{PATH} is required']},
+    cname: {type: String, required: [false, '{PATH} is required']},
     address: {type: String, required: [true, '{PATH} is required'] },
     city: {type: String, required: [true, '{PATH} is required'] },
     postal: {type: Number, min: [0, 'Can not be a negetive value'], required: [true, '{PATH} is required']},
     mobile: {type: Number, min: [0, 'Can not be a negetive value'], required: [true, '{PATH} is required']},
     email: {type: String, required: [true, '{PATH} is required'] },
-    date: {type: Date, required: [true, '{PATH} is required'],default: Date.now },
-    product: {type:[Schema.Types.Mixed], required: [true, '{PATH} is required'] },
+    date: {type: Date, required: [true, '{PATH} is required'], default: Date.now },
+    product: {type:[[String]], required: [true, '{PATH} is required'] },
     //quantity: {type: Number, min: [0, 'Can not be a negetive value'] },
     CGST: {type: [Number]},
     SGST: {type: [Number]},
-    price: {type: Number, default: null}
+    price:{type: Number}
 });
 
 // we need to create a model using it
