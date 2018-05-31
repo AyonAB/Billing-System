0c55de95d92c55d12e01404f36a03a41276f3752
ayantorres <ayantorres@gmail.com>
2018-05-31 12:05:13 -0400

    Minor change in Middleware

--- middleware/middle.js
+++ middleware/middle.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 module.exports = {
     isLoginCheck : function (request, response, next) {
-        if(!request.session.user && request.path != '/index' && request.path != '/forgot-pass' && request.path != '/forgot' && request.path != '/reset/'){
+        if(!request.session.user && request.path != '/index' && request.path != '/forgot-pass' && request.path != '/forgot' && request.path != '/reset/*'){
             response.redirect('/index');
         }else{
             next();
