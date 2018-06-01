d7f95b041b61439ec89fd4b791cba206940dbc73
AyonAB <ayantorres@gmail.com>
2018-06-01 19:03:10 +0530

    Exception added for reset route correctly

--- middleware/middle.js
+++ middleware/middle.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 module.exports = {
     isLoginCheck : function (request, response, next) {
-        if(!request.session.user && request.path != '/index' && request.path != '/forgot-pass' && request.path != '/forgot' && request.path != '/reset/*'){
+        if(!request.session.user && request.path != '/index' && request.path != '/forgot-pass' && request.path != '/forgot' && !request.path.match(/\/reset\/[\w+]/)){
             response.redirect('/index');
         }else{
             next();
