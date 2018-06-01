0d9eef01e26547ce83e3af7ab9522e98d2f92524
Susanta <tricks@Susantas-Mac.local>
2018-06-02 00:46:29 +0530

    exception added for reset pass post method

--- middleware/middle.js
+++ middleware/middle.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 module.exports = {
     isLoginCheck : function (request, response, next) {
-        if(!request.session.user && request.path != '/index' && request.path != '/forgot-pass' && request.path != '/forgot' && !request.path.match(/\/reset\/[\w+]/)){
+        if(!request.session.user && request.path != '/index' && request.path != '/forgot-pass' && request.path != '/forgot' && !request.path.match(/\/reset\/[\w+]/) && request.path != '/reset-post'){
             response.redirect('/index');
         }else{
             next();
