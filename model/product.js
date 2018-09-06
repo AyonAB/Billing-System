edff2258d6d9c2695a1ca684d0b8f567d9984bc8
AyonAB <ayantorres@gmail.com>
2018-09-06 21:45:17 +0530

    Minor Schema changes of product

--- model/product.js
+++ model/product.js
@@ -5,10 +5,10 @@ var Schema = mongoose.Schema;
 var product = new Schema({
     brand:{type: String, required: [true, '{PATH} is required']},
     pname: {type: String, required: [true, '{PATH} is required']},
-    serialno: {type: String, required: [true, '{PATH} is required'] },
+    serialno: {type: String},
     buy: {type: Number, min: [0, 'Can not be a negetive value'], required: [true, '{PATH} is required']},
     sell: {type: Number, min: [0, 'Can not be a negetive value'], required: [true, '{PATH} is required']},
-    Gst: {type: Number, required: [true, '{PATH} is required'] },
+    gst: {type: Number, required: [true, '{PATH} is required'] },
     quantity: {type: Number, min: [0, 'Can not be a negetive value'], required: [true, '{PATH} is required'] }
 });
 
